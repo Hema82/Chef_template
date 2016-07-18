26159227017a7e326d0d4f25442436cee0088878
M614840 <vijay.bangalore@kp.org>
2016-07-18 12:45:26 -0700

    updated the attribute file and .erb

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['package']['name']= 'haproxy'
+default['backend']['port']= '80'
+default['backend']['server']= 'google.com'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,23 @@
+
+
 #
 # Cookbook Name:: chef_template
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# package 'haproxy' do
+
+package node['package']['name'] do
+    action :install
+end
+
+template '/etc/haproxy/haproxy.cfg' do
+    source 'haproxy.cfg.erb'
+    mode '0644'
+    action :create
+end
+
+service 'haproxy' do
+    action [:enable, :start]
+end
